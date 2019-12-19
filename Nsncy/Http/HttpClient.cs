caf434410bdef9a1cdd344b02407b639e8af0a54
anderson luiz pereira <andy2903.alp@gmail.com>
2019-12-19 20:20:09 -0300

    Nancy inserted methods http client

---
+++ Nsncy/Http/HttpClient.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Nsncy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nsncy.Http
+{
+    public class HttpClient : IHttpClient
+    {
+        public string BaseUrlEndPoint { get; private set; }
+        public string ContentJsonPoint { get; private set; }
+
+        internal readonly string AccountNamePoint;
+        internal readonly string EnvironmentPoint;
+        internal readonly string ApiKeyPoint;
+        internal readonly string ApiTokenPoint;
+
+        private IConfiguration _config;
+        public enum BaseUrl
+        {
+            RestStarWars = 0
+        }
+
+        public HttpClient()
+        {
+            this._config = Config.Configurations;
+            this.ChangeUrlBase(BaseUrl.RestStarWars);
+        }
+
+        public void ChangeUrlBase(BaseUrl baseUrl)
+        {
+            switch (baseUrl)
+            {
+                case BaseUrl.RestStarWars:
+                    this.BaseUrlEndPoint = _config.GetSection("MarvelComicsAPI:BaseURL").Value;
+                    this.ContentJsonPoint = _config.GetSection("HttpClient:ValueContentJson").Value;
+                    break;
+            }
+        }
+
+        public Task<string> GetAsync(string url)
+        {
+            return this.GetAsync(url, new CancellationToken(false));
+        }
+
+        public Task<string> GetAsync(string url, CancellationToken cancellationToken)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+
+            {
+                client.BaseAddress = new Uri(this.BaseUrlEndPoint);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return client.GetAsync(url, cancellationToken).Result.Content.ReadAsStringAsync();
+
+            }
+        }
+
+        public Task<HttpResponseMessage> PatchAsync(string url, object patchContent, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), new Uri(this.BaseUrlEndPoint + url))
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(patchContent), Encoding.UTF8, this.ContentJsonPoint)
+            };
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("accept", this.ContentJsonPoint);
+                return client.SendAsync(request, cancellationToken);
+
+            }
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string url, object postContent)
+        {
+            return this.PostAsync(url, postContent, CancellationToken.None);
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string url, object postContent, CancellationToken cancellationToken)
+        {
+            var body = new StringContent(JsonConvert.SerializeObject(postContent), Encoding.UTF8, this.ContentJsonPoint);
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(this.BaseUrlEndPoint);
+                client.DefaultRequestHeaders.Add("accept", this.ContentJsonPoint);
+                return client.PostAsync(url, body, cancellationToken);
+            }
+        }
+
+        public Task<HttpResponseMessage> PutAsync(string url, object putContent)
+        {
+            return this.PutAsync(url, putContent, CancellationToken.None);
+        }
+
+        public Task<HttpResponseMessage> PutAsync(string url, object putContent, CancellationToken cancellationToken)
+        {
+            var body = new StringContent(JsonConvert.SerializeObject(putContent), Encoding.UTF8, this.ContentJsonPoint);
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(this.BaseUrlEndPoint);
+                client.DefaultRequestHeaders.Add("accept", this.ContentJsonPoint);
+
+                return client.PutAsync(url, body, cancellationToken);
+            }
+        }
+    }
+}
---
+++ Nsncy/Http/IHttpClientcs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nsncy.Http
+{
+    public interface IHttpClient
+    {
+        void ChangeUrlBase(HttpClient.BaseUrl baseUrl);
+
+        Task<string> GetAsync(string url);
+        Task<string> GetAsync(string url, CancellationToken cancellationToken);
+
+        Task<HttpResponseMessage> PostAsync(string url, object postContent);
+        Task<HttpResponseMessage> PostAsync(string url, object postContent, CancellationToken cancellationToken);
+
+        Task<HttpResponseMessage> PutAsync(string url, object putContent);
+        Task<HttpResponseMessage> PutAsync(string url, object putContent, CancellationToken cancellationToken);
+
+        Task<HttpResponseMessage> PatchAsync(string url, object patchContent, CancellationToken cancellationToken);
+    }
+}
