dd4b26c42ebf07134936b352cb249d5a7d5a8855
anderson luiz pereira <andy2903.alp@gmail.com>
2019-12-19 20:08:40 -0300

    Nancy start project

---
+++ Nsncy/Controllers/ApiModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace Nsncy.Controllers
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule()
+        {
+            Get("/", args => JsonConvert.SerializeObject("Hello StarWars"));
+        }
+    }
+}
---
+++ Nsncy/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
+using Nsncy.Models;
+
+namespace Nsncy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
+        {
+            Configuration = config;
+
+            Config.Configurations = this.Configuration;
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //app.UseMvc();
+            app.UseOwin(n => n.UseNancy());
+        }
+    }
+}
